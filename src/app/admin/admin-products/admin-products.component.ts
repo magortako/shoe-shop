f11f94352cd67d4a5d0e60f9c68498422f411ac6
magortako <takomagor@gmail.com>
2018-05-15 15:44:32 +0200

    #314- Feeding the table with data

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { Product } from './../../models/product';
 import { ProductService } from './../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from "rxjs/Subscription";
+import { DataTableResource } from "angular5-data-table";
 
 @Component({
   selector: 'app-admin-products',
@@ -12,10 +13,38 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: any[];
   subscription:Subscription;
+  tableResource: DataTableResource<Product>;
+  items : Product[] = [];
+  itemCount:number;
 
+ 
   constructor(private productService: ProductService) { 
     this.subscription = productService.getAll()
-    .subscribe(products => this.filteredProducts = this.products = products);
+    .subscribe(products => {
+      this.filteredProducts = this.products = products;
+      this.initializeTable(products)
+  });
+}
+
+ //Getting all the data into the data-table
+  private initializeTable(products:Product[]){
+
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset:0 })
+    .then(items => this.items = items);
+    this.tableResource.count()
+    .then(count =>this.itemCount = count)
+
+  }
+
+  //On page change or sort change
+  reloadItems(params){
+
+    if(!this.tableResource) return;
+
+    this.tableResource.query({ offset:0 })
+    .then(items => this.items = items);
+
   }
 
   filter(query:string){
