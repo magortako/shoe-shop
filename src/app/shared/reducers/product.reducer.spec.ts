c297ab785e7e398ed01642dbb4314171bcaee1f5
magortako <takomagor@gmail.com>
2018-06-28 15:51:45 +0200

    DeepFreeze unit testing --npm install, and create a spec file to add the layout of the unit test

---
+++ src/app/shared/reducers/product.reducer.spec.ts
@@ -0,0 +1,23 @@
+import { productReducer } from 'shared/reducers/product.reducer';
+import * as types from 'shared/actions/product.actions.ts';
+
+var deepFreeze = require('deep-freeze');
+
+describe('products reducer', () =>{
+    it('should return the initial state', () => {
+        expect(productReducer)
+    })
+});
+
+
+beforeEach(() =>{
+
+});
+
+it('Should ', () => {
+    
+})
+
+it('Should ', () => {
+
+})
