2fa808ba2d876882b488d2ccb3827d6078b6492b
Arash <838@mail.com>
2018-06-29 22:55:08 +0200

    test: product reducer

--- src/app/shared/reducers/product.reducer.spec.ts
+++ src/app/shared/reducers/product.reducer.spec.ts
@@ -1,23 +1,63 @@
-import { productReducer } from 'shared/reducers/product.reducer';
+const deepFreeze = require('deep-freeze');
+
+import { productReducer, initialState, Action } from 'shared/reducers/product.reducer';
 import * as types from 'shared/actions/product.actions.ts';
+import { Product } from 'shared/models/product';
 
-var deepFreeze = require('deep-freeze');
+describe('productsReducer()', () => {
 
-describe('products reducer', () =>{
-    it('should return the initial state', () => {
-        expect(productReducer)
-    })
+  it('should return the initial state', () => {
+
+    expect(productReducer(undefined, { type: null, payload: null })).toEqual(initialState);
+
+  });
+
 });
 
+it('Should loading be true while data gets loaded', () => {
 
-beforeEach(() =>{
+  const stateBeforeMock = initialState;
+
+  const action: Action = { type: types.GET_PRODUCTS, payload: null };
+
+  const stateAfterMock = {
+    products: [],
+    loading: true
+  };
+
+  deepFreeze(stateBeforeMock);
+
+  expect(productReducer(stateBeforeMock, action)).toEqual(stateAfterMock);
 
 });
 
 it('Should ', () => {
-    
-})
 
-it('Should ', () => {
+  const payloadMock: Product[] = [
+    {
+      $key: '$key',
+      category: 'category',
+      imageUrl: 'imageUrl',
+      price: 200,
+      title: 'title',
+    }
+  ];
 
-})
+
+  const stateBeforeMock = {
+    ...initialState,
+    loading: true
+  };
+
+  const action: Action = { type: types.GET_PRODUCTS_SUCCESS, payload: payloadMock };
+
+  deepFreeze(stateBeforeMock);
+
+  const stateAfterMock = {
+    products: payloadMock,
+    loading: false,
+  };
+
+  expect(productReducer(stateBeforeMock, action)).toEqual(stateAfterMock);
+
+});
